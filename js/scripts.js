731725734c87a12121270eb4c7bb5002a463449c
shatha shaath and Shradha Pulla <shradha@>
2016-06-16 10:22:45 -0700

    Add basic js script

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
 //Business Logic
 
+function Player() {
+  this.player1 = "X";
+  this.player2 = "O";
+}
 
-
+Player.prototype.space = function() {
+  var newX = this.player1;
+  var newO = this.player2;
+}
 
 
 
 //User Interface Logic
+$(document).ready(function(event) {
+
+  var newPlayer = new Player();
+
+  $("#s1").click(function(event) {
+    newPlayer.space(this.player1);
+    alert(this.player1);
+  });
+});
