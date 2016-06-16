bfa3ddcb0aa5b37c3a06d34fc46664d63d56f17f
shatha shaath and Shradha Pulla <shradha@>
2016-06-16 10:44:19 -0700

    make all spaces clickable

--- js/scripts.js
+++ js/scripts.js
@@ -20,4 +20,28 @@ $(document).ready(function(event) {
   $("#s1").click(function(event) {
     $("#s1-value").text(newPlayer.player1);
   });
+  $("#s2").click(function(event){
+    $("#s2-value").text(newPlayer.player1);
+  });
+  $("#s3").click (function(event){
+    $("#s3-value").text(newPlayer.player1);
+  });
+  $("#s4").click(function(event) {
+    $("#s4-value").text(newPlayer.player1);
+  });
+  $("#s5").click(function(event) {
+    $("#s5-value").text(newPlayer.player1);
+  });
+  $("#s6").click(function(event) {
+    $("#s6-value").text(newPlayer.player1);
+  });
+  $("#s7").click(function(event) {
+    $("#s7-value").text(newPlayer.player1);
+  });
+  $("#s8").click(function(event) {
+    $("#s8-value").text(newPlayer.player1);
+  });
+  $("#s9").click(function(event) {
+    $("#s9-value").text(newPlayer.player1);
+  });
 });
