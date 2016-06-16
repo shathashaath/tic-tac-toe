bf49aada7525e10546a09542369b423e82473e49
shatha shaath and Shradha Pulla <shatha@>
2016-06-16 15:05:54 -0700

    minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 //Business Logic
-
 function Player(playerMark) {
   this.playerMark = playerMark;
   this.spacesTaken = [];
 }
 
 Player.prototype.turn = function() {
+  // var xPlayer = new Player("X");
 
+  if (=== ) {
+    alert("Now it's the other player's turn");
+  }
 }
 
 
@@ -17,39 +20,41 @@ $(document).ready(function(event) {
   var xPlayer = new Player("X");
   var oPlayer = new Player("O");
 
+
   $("#s1").click(function(event) {
-    $("#s1-value").text();
+    // xPlayer.turn();
+    $("#s1").text(xPlayer.playerMark);
   });
 
   // $("#s2").click(function(event){
-  //   $("#s2-value").text(newPlayer.player1);
+  //   $("#s2").text(oPlayer.playerMark);
   // });
   //
   // $("#s3").click (function(event){
-  //   $("#s3-value").text(newPlayer.player1);
+  //   $("#s3").text(xPlayer.playerMark);
   // });
   //
   // $("#s4").click(function(event) {
-  //   $("#s4-value").text(newPlayer.player1);
+  //   $("#s4").text(oPlayer.playerMark);
   // });
   //
   // $("#s5").click(function(event) {
-  //   $("#s5-value").text(newPlayer.player1);
+  //   $("#s5").text(xPlayer.playerMark);
   // });
   //
   // $("#s6").click(function(event) {
-  //   $("#s6-value").text(newPlayer.player1);
+  //   $("#s6").text(oPlayer.playerMark);
   // });
   //
   // $("#s7").click(function(event) {
-  //   $("#s7-value").text(newPlayer.player1);
+  //   $("#s7").text(xPlayer.playerMark);
   // });
   //
   // $("#s8").click(function(event) {
-  //   $("#s8-value").text(newPlayer.player1);
+  //   $("#s8").text(oPlayer.playerMark);
   // });
   //
   // $("#s9").click(function(event) {
-  //   $("#s9-value").text(newPlayer.player1);
+  //   $("#s9").text(xPlayer.playerMark);
   // });
 });
