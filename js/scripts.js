a5541909ead2eb6375f9955fe33a9fcf08b59e97
shatha shaath and Shradha Pulla <shradha@>
2016-06-16 12:43:04 -0700

    Change constructor function to be more general, add two player variables in UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 //Business Logic
 
-function Player() {
-  this.player1 = "X";
-  this.player2 = "O";
+function Player(playerMark) {
+  this.playerMark = playerMark;
+  this.spacesTaken = [];
 }
 
-Player.prototype.space = function() {
-  var newX = this.player1;
-  var newO = this.player2;
+Player.prototype.turn = function() {
+
 }
 
 
@@ -15,33 +14,42 @@ Player.prototype.space = function() {
 //User Interface Logic
 $(document).ready(function(event) {
 
-  var newPlayer = new Player();
+  var xPlayer = new Player("X");
+  var oPlayer = new Player("O");
 
   $("#s1").click(function(event) {
-    $("#s1-value").text(newPlayer.player1);
+    $("#s1-value").text();
   });
-  $("#s2").click(function(event){
-    $("#s2-value").text(newPlayer.player1);
-  });
-  $("#s3").click (function(event){
-    $("#s3-value").text(newPlayer.player1);
-  });
-  $("#s4").click(function(event) {
-    $("#s4-value").text(newPlayer.player1);
-  });
-  $("#s5").click(function(event) {
-    $("#s5-value").text(newPlayer.player1);
-  });
-  $("#s6").click(function(event) {
-    $("#s6-value").text(newPlayer.player1);
-  });
-  $("#s7").click(function(event) {
-    $("#s7-value").text(newPlayer.player1);
-  });
-  $("#s8").click(function(event) {
-    $("#s8-value").text(newPlayer.player1);
-  });
-  $("#s9").click(function(event) {
-    $("#s9-value").text(newPlayer.player1);
-  });
+
+  // $("#s2").click(function(event){
+  //   $("#s2-value").text(newPlayer.player1);
+  // });
+  //
+  // $("#s3").click (function(event){
+  //   $("#s3-value").text(newPlayer.player1);
+  // });
+  //
+  // $("#s4").click(function(event) {
+  //   $("#s4-value").text(newPlayer.player1);
+  // });
+  //
+  // $("#s5").click(function(event) {
+  //   $("#s5-value").text(newPlayer.player1);
+  // });
+  //
+  // $("#s6").click(function(event) {
+  //   $("#s6-value").text(newPlayer.player1);
+  // });
+  //
+  // $("#s7").click(function(event) {
+  //   $("#s7-value").text(newPlayer.player1);
+  // });
+  //
+  // $("#s8").click(function(event) {
+  //   $("#s8-value").text(newPlayer.player1);
+  // });
+  //
+  // $("#s9").click(function(event) {
+  //   $("#s9-value").text(newPlayer.player1);
+  // });
 });
